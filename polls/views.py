b9ad3b7dadd56a947ec54ea6d9ef3b97baefe601
Igor K. <igor.k@gmail.com>
2025-10-15 19:23:42 +0200

    lesson 39 'HTML, CSS and the basics of markup language'. Part 3 tutorial Django, end

--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
 
 # from .models import Question
 class Question:
-    def __init__(self, question_text):
+    def __init__(self, id, question_text):
+        self.id = id
         self.question_text = question_text
 
-latest_question_list = [Question('question_text_1'), Question('question_text_2'), Question('question_text_3')]
+    def __str__(self):
+        return f'{self.question_text},id={self.id}'
+
+latest_question_list = [Question(1,'question_text_1'), Question(2,'question_text_2'),
+                        Question(3,'question_text_3')]
+# latest_question_list = []
+
+# def index(request):
+#     # latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     output = ", ".join([q.question_text for q in latest_question_list])
+#     return HttpResponse(output)
 
 def index(request):
     # latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template("polls/index.html")
+    context = {"latest_question_list": latest_question_list}
+    return HttpResponse(template.render(context, request))
 
-# def index(request):
-#     return HttpResponse("Hello, world. You're at the polls index.")
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        # question = Question.objects.get(pk=question_id)
+        question = latest_question_list[question_id - 1]
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
