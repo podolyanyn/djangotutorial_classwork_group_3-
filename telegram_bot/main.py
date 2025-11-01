9547b2e0f59910ff38fa63c18c2d29105fb915be
Igor K. <igor.k@gmail.com>
2025-11-01 15:59:12 +0100

    Lesson_46_Telegram API. End lesson.

---
+++ telegram_bot/main.py
@@ -0,0 +1,40 @@
+import asyncio
+import telegram
+
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('8415030792:AAHoTnE63WW6ZtOAPPys46opFpQew2ltumw').build()
+
+    start_handler = CommandHandler('start', start)
+    caps_handler = CommandHandler('caps', caps)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+
+    application.add_handler(start_handler)
+    application.add_handler(caps_handler)
+    application.add_handler(echo_handler)
+    application.add_handler(unknown_handler)
+
+    application.run_polling()
+
